aaea6c9e3b54041df4b844ddec5b477ff1d97cf4
MreRes <47110975+MreRes@users.noreply.github.com>
2025-04-27 18:01:55 +0700

    Add financial-bot/frontend/src/components/common/index.ts 1745751715230

---
+++ financial-bot/frontend/src/components/common/index.ts
@@ -0,0 +1,7 @@
+export { default as Alert } from './Alert';
+export { default as Button, IconButton } from './Button';
+export { default as Card } from './Card';
+export { default as Input, TextArea } from './Input';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as Modal } from './Modal';
+export { default as Select } from './Select';
